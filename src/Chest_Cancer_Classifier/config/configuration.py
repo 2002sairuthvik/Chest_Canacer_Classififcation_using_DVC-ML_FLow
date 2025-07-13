5faef1c02c5365273effef58ae8f51c7dcca8e6b
Sai Ruthvik <msairuthvik@gmail.com>
2025-07-13 18:28:45 -0500

    dvc added

--- src/Chest_Cancer_Classifier/config/configuration.py
+++ src/Chest_Cancer_Classifier/config/configuration.py
@@ -1,9 +1,9 @@
 from Chest_Cancer_Classifier.constants import *
-from Chest_Cancer_Classifier.utils.common import read_yaml,create_directories
+from Chest_Cancer_Classifier.utils.common import read_yaml,create_directories,save_json
 from Chest_Cancer_Classifier.entity.config_entity import DataIngestionConfig
 from Chest_Cancer_Classifier.entity.config_entity import PrepareBaseModelConfig
 import tensorflow as tf
-from Chest_Cancer_Classifier.entity.config_entity import TrainingConfig
+from Chest_Cancer_Classifier.entity.config_entity import TrainingConfig,EvaluationConfig
 import os
 
 
@@ -74,3 +74,14 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/Chest-CT-Scan-data",
+            mlflow_uri="https://dagshub.com/2002sairuthvik/Chest_Canacer_Classififcation_using_DVC-ML_FLow.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
